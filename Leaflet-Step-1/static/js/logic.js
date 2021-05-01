a14019b0dce1215c435dfbe094918d1216d84ea1
cjvilla <CelineVillareal2020@northwestern.edu>
2021-05-01 18:57:28 -0500

    upload functions to logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,4 +1,4 @@
-// Store our API endpoint inside queryUrl
+// store url
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
